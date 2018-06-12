92d54d67e706f9f3bebd6cbd964142b75fae1c8f
Abhilash Kandala <abhi5598@gmail.com>
2018-06-12 11:39:53 -0700

    Update ProgrammingAssignment2.R

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -22,7 +22,7 @@ makeCacheMatrix  <- function(x = matrix()) {
                 getinv = getinv
         )
 }
-#The following function calculates the mean of the special "vector" created with the above function. However, it first checks to see if the mean has already been calculated. If so, it gets the mean from the cache and skips the computation. Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
+#his function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve  <- function(x, ...) {
         invx <- x$getinv()
