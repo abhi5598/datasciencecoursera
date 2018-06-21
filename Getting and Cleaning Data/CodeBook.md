c18866aaace2bd06f669b33b21ff45ee26f5ddd1
abhi5598 <abhi5598@gmail.com>
2018-06-21 14:12:02 -0700

    Updated

---
+++ Getting and Cleaning Data/CodeBook.md
@@ -0,0 +1,9 @@
+Getting and Cleaning Data Course Project
+
+1) Download & read all the files
+2) Merges the training and the test sets to create one data set.
+3) Extracts only the measurements on the mean and standard deviation for each measurement.
+4) Uses descriptive activity names to name the activities in the data set
+5) Appropriately labels the data set with descriptive variable names.
+6) Created a second, independent tidy data set with the average of each variable for each activity and each subject.
+7) Write the data set to the tidy_data.txt file.
--- Getting and Cleaning Data/run_analysis.R
+++ Getting and Cleaning Data/run_analysis.R
@@ -10,35 +10,22 @@ list.files("./UCI HAR Dataset", recursive = TRUE)
 ################## Reading All files
 
 xtrain <- read.table("./UCI HAR Dataset/train/X_train.txt")
-head(xtrain)
-names(xtrain)
 ytrain <- read.table("./UCI HAR Dataset/train/y_train.txt")
-str(ytrain)
 strain <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-names(strain)
 xtest <- read.table("./UCI HAR Dataset/test/X_test.txt")
-names(xtest)
 ytest <- read.table("./UCI HAR Dataset/test/y_test.txt")
-str(ytest)
 stest <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-names(stest)
 features <- read.table('./UCI HAR Dataset/features.txt')
-str(features)
 alabels = read.table('./UCI HAR Dataset/activity_labels.txt')
-head(alabels)
 
 #############    1 Merge files
-head(features)
-
 m1 <- rbind(xtrain, xtest)
 m2 <- rbind(ytrain, ytest)
 m3 <- rbind(strain, stest)
-str(m3)
 fulldata <- cbind(m1, m2, m3)
-names(fulldata)
 colnames(fulldata) <-
         c(as.character(features$V2), "subject", "activity")
-names(fulldata)
+
 ############################# 2 Extract Data
 subfeatures <-
         c(as.character(features$V2[grep("mean\\(\\)|std\\(\\)", features$V2)]), "subject", "activity")
@@ -50,7 +37,9 @@ finaldata <- subset(fulldata, select = subfeatures)
 ############################### 4 descriptive variable names
 finaldata$activity <-
         factor(finaldata$activity, levels = alabels[, 1], labels = alabels[, 2])
-str(finaldata)
+finaldata$subject <- as.factor(finaldata$subject)
 
 ############################### 5 wite file
+tidydata <- aggregate(. ~ subject + activity, finaldata, mean)
 
+write.table(tidydata, "tidydata.txt", row.name = FALSE)
